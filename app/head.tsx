b41d92da66f1789dcb70aa8c5dbb5db47b9b29dd
motodogs-ceo <motodogs@yahoo.com>
2025-06-14 16:14:21 -0400

    fixed site name in tabs

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>Get $WRAK</title>
+      <meta
+        name="description"
+        content="WRAKcoin • no roadmap, no whitelist, no mercy"
+      />
+    </>
+  );
+}
